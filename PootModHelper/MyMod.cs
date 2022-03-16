5609db5effda0fcabfe5420b64f7c88e0445f19e
Thomas Margraff <thomaspmar@protonmail.com>
2022-03-15 22:03:56 -0700

    Updated version

--- PootModHelper/MyMod.cs
+++ PootModHelper/MyMod.cs
@@ -9,7 +9,7 @@ namespace PootModHelper
         public static MyMod Instance { get; private set; }
 
         public const string modName = "Poot Mod Helper";
-        public const string modVersion = "1.0.0.0";
+        public const string modVersion = "0.0.1";
         public const string modAuthor = "Gurrenm4";
 
         public override void OnApplicationStart()
